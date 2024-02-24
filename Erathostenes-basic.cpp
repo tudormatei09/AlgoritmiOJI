89508e91bee324992ca6fc23e37bfa7a06f57c42
tudormatei09 <125114789+tudormatei09@users.noreply.github.com>
2024-02-24 22:00:53 +0200

    Update Erathostenes-basic.cpp

--- Erathostenes-basic.cpp
+++ Erathostenes-basic.cpp
@@ -1,7 +1,5 @@
 #include "bits/stdc++.h"
-
 std::vector<bool>prime(1000001,true);
-
 void ciur(){
     prime[0] = prime[1] = false;
     for(int i = 2; i*i <= 1000000; i++){
@@ -11,19 +9,4 @@ void ciur(){
            }
        }
     }
-}
-
-
-int main(){
-      ciur();
-     std::ios_base::sync_with_stdio(false);
-     std::cin.tie(0);
-     int n;
-     std::cin >> n;
-     for(int i = 0; i < n; i++){
-          if(prime[i] == true){
-             std::cout << i << " ";
-          }
-     }
-    return 0;
 }
