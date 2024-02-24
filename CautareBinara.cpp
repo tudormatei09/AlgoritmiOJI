cf29d8811d344c0cbc99253e204bf52feaaf3615
tudormatei09 <125114789+tudormatei09@users.noreply.github.com>
2024-02-24 22:00:23 +0200

    Update CautareBinara.cpp

--- CautareBinara.cpp
+++ CautareBinara.cpp
@@ -1,5 +1,3 @@
-#include "bits/stdc++.h"
-
 int cb(std::vector<int>&v,int n){
     int st = 0;
     int dr = v.size();
@@ -16,27 +14,4 @@ int cb(std::vector<int>&v,int n){
        }
     }
   return false;
-}
-
-int main(){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(0);
-    int n;
-    std::cin >> n;
-    std::vector<int>v;
-    for(int i = 0; i < n; i++){
-        int x;
-        std::cin >> x;
-        v.push_back(x);
-    }
-    std::sort(v.begin(),v.end());
-    int m;
-    std::cin >> m;
-    for(int i = 0; i < m; i++){
-    int x;
-        std::cin >> x;
-        std::cout << cb(v,x) << " ";
-    }
-
-     return 0;
 }
