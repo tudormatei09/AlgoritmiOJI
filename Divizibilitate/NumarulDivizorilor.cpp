3a9fd262511328d2d64bcb9fc19e98ce0cbaa4da
Holotă Tudor-Matei <125114789+tudormatei09@users.noreply.github.com>
2024-04-07 20:43:10 +0300

    Update NumarulDivizorilor.cpp

--- Divizibilitate/NumarulDivizorilor.cpp
+++ Divizibilitate/NumarulDivizorilor.cpp
@@ -15,3 +15,28 @@ long long numberOfDivisors(long long num) {
     }
     return total;
 }
+
+
+/////sau ciur
+#include "bits/stdc++.h"
+const int NMAX = 1e6;
+std::vector<int>nrdiv(NMAX,0);
+inline static void task(){
+     for(int i = 1; i <= NMAX+6; i++){
+            for(int j = i; j <= NMAX; j=j+i){
+                 nrdiv[j]++;
+            }
+     }
+     int n;
+     std::cin >> n;
+     std::cout <<  nrdiv[n];
+}
+signed main(){
+     freopen("nrdiv.in","r",stdin);
+     freopen("nrdiv.out","w",stdout);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    task();
+    return 0;
+}
