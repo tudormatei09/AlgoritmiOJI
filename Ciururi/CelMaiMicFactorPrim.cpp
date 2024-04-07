22afec67a08dabab36441967aecabe5df3feea8e
Holotă Tudor-Matei <125114789+tudormatei09@users.noreply.github.com>
2024-04-07 20:59:31 +0300

    Create CelMaiMicFactorPrim.cpp

---
+++ Ciururi/CelMaiMicFactorPrim.cpp
@@ -0,0 +1,8 @@
+int M[DIM + 1];
+for(int i = 1 ; i <= DIM ; i ++)
+    M[i] = i;
+for(int i = 2; i <= DIM ; i ++)
+    if(M[i] == i)
+        for(int j = 2 ; i * j <= DIM ; j ++)
+            if(M[i * j] == i * j)
+                M[i * j] = i;
